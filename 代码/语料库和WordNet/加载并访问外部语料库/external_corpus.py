79402fec2a39f577d7627642692f404e7818df13
daidenghui <3276336032@qq.com>
2020-06-29 01:10:04 +0800

    加载外部库 根据加载内容适当修改代码

--- 代码/语料库和WordNet/加载并访问外部语料库/external_corpus.py
+++ 代码/语料库和WordNet/加载并访问外部语料库/external_corpus.py
@@ -10,10 +10,17 @@ from nltk.corpus import CategorizedBracketParseCorpusReader
 from random import randint
 
 # 读取文件
-reader = CategorizedBracketParseCorpusReader(r'E:/课程/自然语言处理/代码/语料库和WordNet/加载并访问外部语料库/tokens/neg',
-                                             r'.*\txt', cat_pattern=r'(\w)/*')
+reader = CategorizedBracketParseCorpusReader(r'F:/Downloads/NLP文件/mix20_rand700_tokens_cleaned/tokens',
+                                             r'.*\.txt', cat_pattern=r'(\w+)/*')
 print(reader.categories())
 print(reader.fileids())
 
-posFiles = reader.fileids(categories='pos')
 negFiles = reader.fileids(categories='neg')
+
+fileN = negFiles[randint(0, len(negFiles)-1)]
+print(fileN)
+
+for w in reader.words(fileN):
+    print(w + ' ', end=" ")
+    if w is '.':
+        print()
